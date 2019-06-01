7578cc85349b853a77b201119f0d26d396abf59e
Kenshiro <sato-kenshiro906@g.ecc.u-tokyo.ac.jp>
2019-06-01 17:21:16 +0900

    first commit

---
+++ mlmodules.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+get_ipython().run_line_magic('matplotlib', 'notebook')
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+from IPython.display import display
+import pandas as pd
+
+
+# In[ ]:
+
+
+
+
